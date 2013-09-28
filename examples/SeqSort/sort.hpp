53acc01435231f3122ebceab5b247c10209cc619
Веселов Андрей <github.coffee@hotmail.com>
2013-09-28 16:51:20 +0400

    minor fix

--- examples/SeqSort/sort.hpp
+++ examples/SeqSort/sort.hpp
@@ -1,36 +1,47 @@
 #include <async/async_fwd.hpp>
 
-template<typename RandomAccessIteratorT>
+// boost headers
+#include <boost/asio/io_service.hpp>
+#include <boost/function.hpp>
+#include <boost/ref.hpp>
+#include <boost/bind.hpp>
+
+// stl headers
+#include <algorithm>
+
+template<typename RandomAccessIteratorT> inline
 void merge(
       RandomAccessIteratorT theBegin
     , RandomAccessIteratorT theMiddle
     , RandomAccessIteratorT theEnd
     , boost::function<void()> theCallback
+)
 {
     std::inplace_merge(theBegin, theMiddle, theEnd);
     theCallback();
 }
 
-template<typename RandomAccessIteratorT>
+template<typename RandomAccessIteratorT> inline
 void sort(
       boost::asio::io_service &theService
-      RandomAccessIteratorT theBegin
+    , RandomAccessIteratorT theBegin
     , RandomAccessIteratorT theEnd
     , boost::function<void()> theCallback
+)
 {
     size_t size = theEnd - theBegin;
     
     if (size <= 4096) // block size 4k bytes
     {
-        std::sort(theBegin, theEnd)
+        std::sort(theBegin, theEnd);
     }
     else
     {
         RandomAccessIteratorT middle = theBegin + size / 2;
         
         async<void(), void()>(
-              boost::bind(&sort<RandomAccessIteratorT>, boost::ref(theService) theBegin, middle, _1)
-            , boost::bind(&sort<RandomAccessIteratorT>, boost::ref(theService), middle, theEnd, _1)
+              boost::bind(&sort<RandomAccessIteratorT>, boost::ref(theService) theBegin, middle, boost::_1)
+            , boost::bind(&sort<RandomAccessIteratorT>, boost::ref(theService), middle, theEnd, boost::_1)
             , boost::bind(&merge<RandomAccessIteratorT>, theBegin, theEnd, theCallback)
         );
     }
