90c947603c8dddb7022fbd884b8aee167ca3ec09
NOMORECOFFEE <github.coffee@hotmail.com>
2013-10-07 00:07:23 +0400

    Update async.hpp

--- include/async/async.hpp
+++ include/async/async.hpp
@@ -1,54 +1,108 @@
-#ifndef ASYNC_INCLUDED_HPP
-#define ASYNC_INCLUDED_HPP 1
+#if BOOST_PP_IS_ITERATING
+    #define ASYNC_PP_ITERATION \
+        BOOST_PP_ITERATION()
 
-// boost headers
-#include <boost/optional/optional_fwd.hpp>
-
-// declaration, forward
-template<class T>
-struct RemoveRef;
-
-template<typename SignT>
-struct ParameterTypes;
-
-struct RemoveRef
-{
-    template<class SingT> struct result;
+    #define ASYNC_PP_typename_T \
+        BOOST_PP_ENUM_PARAMS(BOOST_PHOENIX_ITERATION, typename T)
     
-    template<class T>
-    struct result<void(T)>
+    #define ASYNC_PP_typename_A \
+        BOOST_PP_ENUM_PARAMS(BOOST_PHOENIX_ITERATION, typename A)
+
+    template<ASYNC_PP_typename_T, ContinuationF>
+    struct async_state
     {
-        typedef typename RemoveRef<T>::Type type;    
-    };
-    
-    template<class T>
-    typename RemoveRef<T>::type operator()(T theValue) const
-    {
-        return theValue
+        mutable int m_count;
+        ContinuationF m_callback;
+      
+        explicit async_state(ContinuationF theCallback)
+            : m_count( ASYNC_PP_ITERATION )
+            , m_callback( theCallback )
+        {
+        }
     }
     
-};
-
-struct SetPack
-{
-    template<class SignT>
-    struct result
+    template<int i, typename AsyncStateT>
+    struct task_with_continuation
     {
-        typedef typename ParameterTypes<SignT>::Type type;
-    };
-}
-
-template<ASYNC_typename_T, ASYNC_typename_A, ContinuationF>
-void async(boost::asio::io_service &theService, ASYNC_A_const_ref_a, ContinuationF onComplite)
-{
-    async_invoke(theService, ASYNC_args, theService.wrap( onComplite ));   
-}
-
-template<ASYNC_typename_C, ASYNC_typename_A, ContinuationF>
-void async_invoke(boost::asio::io_service &theService, ASYNC_A_const_ref_a, ContinuationF onComplite)
-{
+        explicit task_with_continuation(boost::shared_ptr<AsyncStateT> theState)
+            : m_state( theState )
+        {
+        }
+        
+        template<typename SeqT>
+        void operator()(SeqT theSeq) const
+        {
+            int volatile *counter = &(m_state->m_counter);
+            
+            if (0 == __sync_fetch_and_sub(counter, 1))
+            {
+                invoke_continuation( m_state );
+            }
+        }
+    private:
+        boost::shared_ptr<AsyncStateT> m_state;
+    }
     
-}
-
-
-#endif // ASYNC_INCLUDED_HPP
+    template<ASYNC_PP_typename_T, ASYNC_PP_typename_A, typename ContinuationF>
+    void async(boost::asio::io_service &theService, ASYNC_PP_A_a, ContinuationF onComplite)
+    {
+        async_invoke(theService, ASYNC_PP_a, theService.wrap( onComplite ));   
+    }
+    
+    template<ASYNC_PP_typename_T, ASYNC_PP_typename_A, typename ContinuationF>
+    void async_invoke(boost::asio::io_service &theService, ASYNC_PP_A_a, ContinuationF onComplite)
+    {
+        typedef async_state<ASYNC_PP_A, ContinuationF>;
+        
+        boost::shared_ptr<
+            async_state<ASYNC_PP_A, ContinuationF>
+        > state = boost::make_shared( onComplite );
+        
+        ASYNC_PP_POST_TASKS
+    }
+#else // BOOST_PP_IS_ITERATING
+    #ifndef ASYNC_INCLUDED_HPP
+    #define ASYNC_INCLUDED_HPP 1
+    
+    // boost headers
+    #include <boost/optional/optional_fwd.hpp>
+    #include <boost/preprocessor/iteration/iterate.hpp>
+    
+    // declaration, forward
+    template<class T>
+    struct RemoveRef;
+    
+    template<typename SignT>
+    struct ParameterTypes;
+    
+    struct RemoveRef
+    {
+        template<class SingT> struct result;
+        
+        template<class T>
+        struct result<void(T)>
+        {
+            typedef typename RemoveRef<T>::Type type;    
+        };
+        
+        template<class T>
+        typename RemoveRef<T>::type operator()(T theValue) const
+        {
+            return theValue
+        }
+        
+    };
+    
+    struct SetPack
+    {
+        template<class SignT>
+        struct result
+        {
+            typedef typename ParameterTypes<SignT>::Type type;
+        };
+    }
+    
+    #define BOOST_PP_ITERATION_PARAMS_1 (3, (1, 10, "async.hpp"))
+    #include BOOST_PP_ITERATE()
+    #endif // ASYNC_INCLUDED_HPP
+#endif // BOOST_PP_IS_ITERATING
