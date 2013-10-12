3932b715436cec00ea97f3036fb6da61346391ee
Веселов Андрей <github.coffee@hotmail.com>
2013-10-12 18:27:41 +0400

    async_invoke recursive

--- include/async/async.hpp
+++ include/async/async.hpp
@@ -2,6 +2,8 @@
 #define ASYNC_HPP_INCLUDED 1
 
 // boost headers
+#include <boost/fusion/container/generation/make_vector.hpp>
+#include <boost/fusion/functional/generation/make_fused_procedure.hpp>
 #include <boost/optional/optional_fwd.hpp>
 #include <boost/preprocessor/iteration/iterate.hpp>
 
@@ -12,26 +14,8 @@ struct RemoveRef;
 template<typename SignT>
 struct ParameterTypes;
 
-struct RemoveRef
-{
-    template<class SingT> struct result;
-
-    template<class T>
-    struct result<void(T)>
-    {
-        typedef typename RemoveRef<T>::Type type;
-    };
-
-    template<class T>
-    typename RemoveRef<T>::type operator()(T theValue) const
-    {
-        return theValue
-    }
-
-};
-
 template<typename SignT, int id>
-struct arguments_list
+struct ArgumentsList
 {
     typedef typename ParameterTypes<SignT>::Type Type;
 
@@ -47,58 +31,83 @@ struct arguments_list
 #include <async/preprocessor/iterate_define.hpp>
 
 template<ASYNC_PP_typename_T, ContinuationF>
-struct async_state
+struct AsyncState
 {
     mutable int m_count;
     ContinuationF m_callback;
   
-    explicit async_state(ContinuationF theCallback)
+    ArgumentsList<ASYNC_PP_T> m_arguments;
+
+    explicit AsyncState(ContinuationF theCallback)
         : m_count( ASYNC_PP_ITERATION )
         , m_callback( theCallback )
+        , m_arguments()
     {
     }
 }
 
 #define ASYNC_PP_base_from_member_DEF(z, n, type) \
-    arguments_list<BOOST_PP_CAT(type, n), n>
+    ArgumentsList<BOOST_PP_CAT(type, n), n>
 
 template<ASYNC_PP_typename_T>
-struct arguments_list
+struct ArgumentsList
     : BOOST_PP_REPEAT(ASYNC_PP_ITERATION, ASYNC_PP_base_from_member_DEF, T)
 {
 };
 
 #undef ASYNC_PP_base_from_member_DEF
 
-template<int i, typename AsyncStateT>
-struct task_with_continuation
+template<int i, ASYNC_PP_typename_T>
+ArgumentsList<i, ASYNC_PP_typename_T> &getReference(ArgumentsList<i, ASYNC_PP_typename_T> &theRef)
 {
-    explicit task_with_continuation(boost::shared_ptr<AsyncStateT> theState)
+    return theRef;
+}
+
+template<int id, typename PtrT>
+struct TaskWithContinuation
+{
+    typedef boost::fusion::fused_procedure<TaskWithContinuation> Type;
+
+    explicit TaskWithContinuation(PtrT const &theState)
         : m_state( theState )
     {
     }
-    
+
     template<typename SeqT>
     void operator()(SeqT theSeq) const
     {
+        getReference<id>( m_state->m_arguments ) = theSeq;
+
         int volatile *counter = &(m_state->m_counter);
-        
+
         if (0 == __sync_fetch_and_sub(counter, 1))
         {
-            invoke_continuation( m_state );
+            invokeContinuation( m_state );
         }
     }
 private:
-    boost::shared_ptr<AsyncStateT> m_state;
+    PtrT m_state;
 }
 
-template<ASYNC_PP_typename_T, ASYNC_PP_typename_A, typename ContinuationF>
+template<int id, class PtrT>
+typename TaskWithContinuation<id, PtrT>::Type inline
+buildContinuationTask(PtrT const &theState)
+{
+    return TaskWithContinuation<id, PtrT>( theState );
+}
+
+template<class AsyncState>
+void invokeContinuation(boost::shared_ptr<AsyncState> &theState)
+{
+}
+
+template<ASYNC_PP_typename_T, ASYNC_PP_typename_A, typename ContinuationF> inline
 void async(boost::asio::io_service &theService, ASYNC_PP_A_a, ContinuationF onComplite)
 {
     async_invoke(theService, ASYNC_PP_a, theService.wrap( onComplite ));   
 }
 
-template<ASYNC_PP_typename_T, ASYNC_PP_typename_A, typename ContinuationF>
+template<ASYNC_PP_typename_T, ASYNC_PP_typename_A, typename ContinuationF> inline
 void async_invoke(boost::asio::io_service &theService, ASYNC_PP_A_a, ContinuationF onComplite)
 {
     typedef async_state<ASYNC_PP_A, ContinuationF>;
@@ -107,7 +116,22 @@ void async_invoke(boost::asio::io_service &theService, ASYNC_PP_A_a, Continuatio
         async_state<ASYNC_PP_A, ContinuationF>
     > state = boost::make_shared( onComplite );
     
-    ASYNC_PP_POST_TASKS
+    async_invoke<ASYNC_PP_ITERATION>(
+        theService, boost::fusion::make_vector(ASYNC_PP_a), state
+    );
+
+}
+
+template<SeqT, typename PtrT> inline
+void async_invoke(boost::asio::io_service &theService, SeqT theSeq, PtrT theState)
+{
+    using boost::fusion::at;
+
+    theService.post(
+        boost::bind(at<i>(theSeq), buildContinuationTask<i>(theState)
+    );
+
+    async_invoke<i-1>(theService, t, theState);
 }
 
 #endif // ASYNC_HPP_INCLUDED
